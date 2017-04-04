5c45ad98cd18b5c09b49dc227cd592df0acdcc52
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2017-04-04 23:39:51 +0200

    Stuff that doesn't work
    console command: python manage.py makemigrations transaction

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Transaction, Currency
+
+admin.site.register(Transaction)
+admin.site.register(Currency)
--- transaction/models.py
+++ transaction/models.py
@@ -2,18 +2,23 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Transaction(models.Model):
-    description = models.TextField
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
-    currency = models.TextField
-    # TODO why?
-    debtor = models.ForeignKey(User, related_name='debtor')
-    creditor = models.ForeignKey(User, related_name='creditor')
-    date_created = models.DateTimeField('date published')
-    deleted = models.BooleanField
-    date_deleted = models.DateTimeField('date published')
-
-
 # TODO move
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+# TODO move
+class Currency(models.Model):
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=3)
+
+
+class Transaction(models.Model):
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    currency = models.ForeignKey(Currency, related_name='currency')
+    debtor = models.ForeignKey(User, related_name='debtor')
+    creditor = models.ForeignKey(User, related_name='creditor')
+    date_created = models.DateTimeField('date_created')
+    deleted = models.BooleanField()
+    date_deleted = models.DateTimeField('date_deleted')
