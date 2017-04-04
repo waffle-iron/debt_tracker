85f6f05fa46c90a911edb9baf05fc7c9537d7e03
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2017-04-04 11:08:07 +0200

    Renamed function

--- debug/packages_view.py
+++ debug/packages_view.py
@@ -5,13 +5,14 @@ from django.http import HttpResponse
 def getView():
     html = "<h1>Required Packages:</h1>"
 
-    for package in getPackages():
+    for package in get_packages():
         html += "<p>%s</p>" % package
 
     return HttpResponse(html)
 
 
-def getPackages():
+def get_packages():
     installed_packages = pip.get_installed_distributions()
     installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+
     return installed_packages_list
