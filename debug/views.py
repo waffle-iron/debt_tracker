674aa8fd1aa85c334e494d691b50b91c9c1ccd05
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2017-04-10 08:48:49 +0200

    requirements view can only be accessed by superuser

--- debug/views.py
+++ debug/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render_to_response
 
 
 def requirements(request):
-    installed_packages = sorted(pip.get_installed_distributions())
-    installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+    # TODO needs redirect or error message
+    if request.user.is_superuser:
+        installed_packages = sorted(pip.get_installed_distributions())
+        installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
 
-    response = render_to_response('debug/requirements.html', locals())
+        response = render_to_response('debug/requirements.html', locals())
 
-    return response
+        return response
